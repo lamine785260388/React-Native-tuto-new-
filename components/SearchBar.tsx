5c851743f1cd45b3538377779387c429a91b7026
lamine785260388 <lamine785260388@gmail.com>
2026-01-05 15:07:18 +0000

    navigation & route

---
+++ components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { icons } from '@/constants/icons'
+import React from 'react'
+import { Image, StyleSheet, TextInput, View } from 'react-native'
+
+const SearchBar = () => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        placeholder="Search"
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({})
