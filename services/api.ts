ff4a77ab3b7fae04ff49f2f49a02dd5fedc80f70
lamine785260388 <lamine785260388@gmail.com>
2026-01-12 16:13:53 +0000

    dd

--- services/api.ts
+++ services/api.ts
@@ -6,7 +6,21 @@ export const TMTB_CONFIG = {
         Authorization:`Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
     }
 }
+export const fetchMovies= async({query}:{query:string}) => {
+    const endpoint= query
+        ? `/search/movie?query=${encodeURIComponent(query)}`
+       : '/discover/movie?sort_by=popularity.desc';
+    const response = await fetch(endpoint,{
+        method:'GET',
+        headers:TMTB_CONFIG.headers,
+    });
+    if(!response.ok){
+        throw new Error(`Failed to fetch movies: ${response.statusText}`);
 
+    }
+    const data = await response.json();
+    return data.results;
+}
 
 // const url = 'https://api.themoviedb.org/3/keyword/keyword_id/movies?include_adult=false&language=en-US&page=1';
 // const options = {
